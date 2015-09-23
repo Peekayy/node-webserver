f6fdaa070b33b7e88f2947888fc660b64f0c0524
gael bougannne (WTV) <gael.bouganne@wiztivi.com>
2015-09-23 10:25:39 +0200

    Added remote address to morgan logs

--- http-server.js
+++ http-server.js
@@ -14,7 +14,31 @@ var argv = require("yargs")
     .alias("c", "cors")
     .argv;
 
-app.use(morgan(":remote-addr :method :url :status :response-time ms - :res[content-length]"));
+morgan.format("custom", function customFormat(tokens, req, res) {
+    // get the status code if response written
+    var status = res._header
+        ? res.statusCode
+        : undefined;
+
+    // get status color
+    var color = status >= 500 ? 31 // red
+        : status >= 400 ? 33 // yellow
+        : status >= 300 ? 36 // cyan
+        : status >= 200 ? 32 // green
+        : 0;// no color
+
+    // get colored function
+    var fn = customFormat[color];
+
+    if (!fn) {
+        // compile
+        fn = customFormat[color] = morgan.compile("\x1b[0m:remote-addr :method :url \x1b[" + color + "m:status \x1b[0m:response-time ms - :res[content-length]\x1b[0m");
+    }
+
+    return fn(tokens, req, res);
+});
+
+app.use(morgan("custom"));
 
 
 var headers = [];
